794f5a7cb38024f52e414ed9f99a9ccc48b08807
CargoX <nikatajaged@gmail.com>
2026-07-03 12:40:14 +0300

    Update opts.h

--- includes/opts.h
+++ includes/opts.h
@@ -1,6 +1,6 @@
 #pragma once
 
-constexpr const char *BACKEND_URL = "http://192.168.1.69:8080";
+constexpr const char *BACKEND_URL = "https://ventex.ogfn.website";
 static const char* EPIC_DOMAINS[] = {
     "game-social.epicgames.com",
     "ol.epicgames.com",
